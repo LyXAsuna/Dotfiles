2257c489ab538d7da0db843cc7c04690bfab3e18
LyXAsuna <66867758+LyXAsuna@users.noreply.github.com>
2021-11-15 00:33:36 +0200

    Delete .config directory

--- .config/polybar/modules/system-nvidia-smi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-string=$(cat ~/.cache/wal/colors | head -2 | tail -1)
-
-if ! gpuuti=$(nvidia-smi --format=nounits,csv,noheader --query-gpu=utilization.gpu | xargs echo); then
-    gpuuti=0
-fi
-if [ "$gpuuti" -gt 0 ]; then
-    echo "$gpuuti%"
-fi
